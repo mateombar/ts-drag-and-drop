2711fbcb394075804478fed64f1d67c6c45e08e5
Mateo Montanez Baron <mateomontabaron@gmail.com>
2024-03-18 13:11:24 -0500

    Getting inputs values

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
 // autobind decorator
-function autobind(
-  _: any,
-  _2: string,
-  descriptor: PropertyDescriptor
-) {
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
@@ -51,11 +47,29 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    return [enteredTitle, enteredDescription, Number(enteredPeople)];
+  }
+
+  private clearInputs() {
+    this.descriptionInputElement.value = "";
+    this.titleInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
-    
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
